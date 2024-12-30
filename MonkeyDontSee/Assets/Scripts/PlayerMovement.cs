b7820deb6581109eccb784e2feb1975e58e70881
Valfezant <valeria.passarella2003@gmail.com>
2024-12-30 16:34:30 +0100

    Climb walls

--- MonkeyDontSee/Assets/Scripts/PlayerMovement.cs
+++ MonkeyDontSee/Assets/Scripts/PlayerMovement.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontalMove;
-    //Controllable
+    private float verticalMove;
+
+    private float normGravityScale;
+    
+    [Header("Stats")]
     public float moveSpeed;
     public float jumpPower;
     public float doubleJumpPower;
+    public float climbingSpeed;
 
+    [Header("Utility")]
     [SerializeField] private Rigidbody2D rb;
-
-    private bool _doubleJump;
-    private bool _facingRight = true;
-    private bool _onGround;
 
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    
+    private bool _doubleJump;
+    private bool _facingRight = true;
+    private bool _onGround;
+    private bool _isLadder;
+    private bool _isClimbing;
 
+    void Start()
+    {
+        normGravityScale = rb.gravityScale;
+    }
     
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+
         Flip();
 
-
-        //jumping
+        //JUMPING
         if (_onGround && !Input.GetButton("Jump"))
         {
             _doubleJump = false;
@@ -46,11 +59,29 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
+
+        //CLIMBING
+        if (_isLadder && Input.GetButton("Fire3"))
+        {
+            _isClimbing = true;
+            _onGround = true;
+        }
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontalMove * moveSpeed, rb.velocity.y);
+
+        if (_isClimbing && Input.GetButton("Fire3"))
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, verticalMove * climbingSpeed);
+        }
+        else
+        {
+            rb.gravityScale = normGravityScale;
+            //_onGround = false;
+        }
 
         CheckCollision();
     }
@@ -67,8 +98,31 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    //collisions
     private void CheckCollision()
     {
-        _onGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        if (!_isClimbing)
+        {
+            _onGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Climbable"))
+        {
+            _isLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Climbable"))
+        {
+            _isLadder = false;
+            _isClimbing = false;
+            _onGround = true;
+        }
     }
 }
