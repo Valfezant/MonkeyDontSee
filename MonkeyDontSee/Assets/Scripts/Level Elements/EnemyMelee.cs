f613aa5848131a2c56ee4dff03780b696c008a99
Valfezant <valeria.passarella2003@gmail.com>
2025-01-01 13:27:13 +0100

    Melee Enemy (Behaviour)

---
+++ MonkeyDontSee/Assets/Scripts/Level Elements/EnemyMelee.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMelee : MonoBehaviour
+{
+    [SerializeField] private GameObject pointA;
+    [SerializeField] private GameObject pointB;
+    
+    [SerializeField] private float enemySpeed;
+    [SerializeField] private float enemyDamage;
+
+    private Rigidbody2D enemyRb;
+    private Transform currentPoint;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
+    }
+
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+
+        if (currentPoint == pointB.transform)
+        {
+            enemyRb.velocity = new Vector2(enemySpeed, 0);
+        }
+        else
+        {
+            enemyRb.velocity = new Vector2(-enemySpeed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            Flip();
+            currentPoint = pointA.transform;
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            Flip();
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            var player = other.GetComponent<PlayerState>();
+            player.playerHealth -= enemyDamage;
+
+            var playerRb = other.GetComponent<Rigidbody2D>();
+            playerRb.AddForce(transform.up * 5f, ForceMode2D.Impulse);
+        }
+    }
+}
