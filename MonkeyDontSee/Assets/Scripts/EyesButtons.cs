1806657f5435558f9aa2d3a1582245ad14cfd9e3
Valfezant <valeria.passarella2003@gmail.com>
2025-01-17 23:47:24 +0100

    Eye tooltip & Sacrifice screen layout

--- MonkeyDontSee/Assets/Scripts/EyesButtons.cs
+++ MonkeyDontSee/Assets/Scripts/EyesButtons.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
 
-public class EyesButtons : MonoBehaviour
+public class EyesButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] public Eye thisEye;
     [SerializeField] private EyesManager eyesManager;
 
     private bool _eyeSelected;
 
+    //Tooltips
+    [SerializeField] public RectTransform tooltipBg;
+    [SerializeField] public TextMeshProUGUI tooltipText;
+
     void Start()
     {
         _eyeSelected = false;
+
+        HideTooltip();
     }
 
     //On "eye" buttons
@@ -74,5 +82,31 @@ public class EyesButtons : MonoBehaviour
             sprite = thisEye.eyeOpen;*/
             GetComponent<Button>().interactable = true;
         }
+    }
+
+    //SHOWING TOOLTIP
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ShowTooltip();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        HideTooltip();
+    }
+
+    private void ShowTooltip()
+    {
+        tooltipText.text = thisEye.eyeDescription;
+
+        //tooltipBg.sizeDelta = new Vector2(tooltipText.preferredWidth, tooltipText.preferredHeight > 500 ? 500 : tooltipText.preferredHeight);
+        tooltipBg.gameObject.SetActive(true);
+        tooltipBg.transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y + 100);
+    }
+
+    private void HideTooltip()
+    {
+        tooltipText.text = "";
+        tooltipBg.gameObject.SetActive(false);
     }
 }
